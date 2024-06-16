4d828c5be12dde3317d0d182efb79588aea6e26f
LE-PLATINUM <jean.registre@free.fr>
2024-06-16 17:26:18 +0200

    update 16/06/2024-17:26:18,33

--- togit.bat
+++ togit.bat
@@ -1,12 +1,9 @@
 @echo OFF
 echo.
+echo Envoi des modifications
+echo.
 git init
 git add .
-REM git remote add origin https://github.com/aplus-services/lyrae-v14.git
-REM Les deux lignes ci-dessous ne sont probablement pas toujours utiles
-REM git rm -r --cached .next
-REM git rm -r --cached node_modules
-REM Les deux lignes ci-dessus ne sont probablement pas toujours utiles
 git commit -m "update %date%-%time%"
 git branch -M main
 git remote add origin https://github.com/LE-PLATINUM/accueil.git
