5c124837f85348ec5a642d6aac658566e13345fe
LE-PLATINUM <jean.registre@free.fr>
2024-06-16 17:19:55 +0200

    update 16/06/2024-17:19:55,12

--- togit.bat
+++ togit.bat
@@ -8,6 +8,6 @@ REM git rm -r --cached .next
 REM git rm -r --cached node_modules
 REM Les deux lignes ci-dessus ne sont probablement pas toujours utiles
 git commit -m "update %date%-%time%"
-REM git branch -M main
-REM git remote add origin https://github.com/LE-PLATINUM/accueil.git
+git branch -M main
+git remote add origin https://github.com/LE-PLATINUM/accueil.git
 git push -u origin main
